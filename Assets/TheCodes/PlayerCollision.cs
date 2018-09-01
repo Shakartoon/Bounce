6c1b820fea3d43e0a7d6079d5a33abcdbc50a931
Unknown <Sm7684@nyu.edu>
2018-09-01 17:20:19 -0400

    final additions
    which are not much

--- Assets/TheCodes/PlayerCollision.cs
+++ Assets/TheCodes/PlayerCollision.cs
@@ -6,76 +6,96 @@ public class PlayerCollision : MonoBehaviour {
 
 //	public GameObject objeckt;  
 
+//	public ParticleSystem.TriggerModule sparkle;
+//	public Transform sparkle; 
+
+	void Start () {
+
+//		sparkle.GetComponent<ParticleSystem> ().enableEmission = false;   
+
+	} 
+
+
 	void OnCollisionEnter2D (Collision2D col) {
 		Debug.Log ("Collision Detected.");
+
+//		sparkle.GetComponent<ParticleSystem> ().enableEmission = true; 
+//		StartCoroutine (stopSparkles); 
+
 		if (col.collider.tag == "pink_rect") {
 		
 			Debug.Log ("COLLIDED!"); 
 
-			Services.audioManager.Play("drumloop1"); 
+			Services.audioManager.Play ("drumloop1"); 
 			 
 		} 
 
 		if (col.collider.tag == "lavendar_rect") {
 
-			Services.audioManager.Play("icecreamsound"); 
+			Services.audioManager.Play ("icecreamsound"); 
 
 		} 
 	
 		if (col.collider.tag == "cy1") {
 
-			Services.audioManager.Play("synth1"); 
+			Services.audioManager.Play ("synth1"); 
 
 		} 
 
 		if (col.collider.tag == "cy2") {
 
-			Services.audioManager.Play("synth2"); 
+			Services.audioManager.Play ("synth2"); 
 
 		} 
 
 		if (col.collider.tag == "cy3") {
 
-			Services.audioManager.Play("synth3"); 
+			Services.audioManager.Play ("synth3"); 
 
 		} 
 
 		if (col.collider.tag == "cy4") {
 
-			Services.audioManager.Play("synth4"); 
+			Services.audioManager.Play ("synth4"); 
 
 		} 
 
 		if (col.collider.tag == "cdg1") {
 
-			Services.audioManager.Play("humming"); 
+			Services.audioManager.Play ("humming"); 
 
 		} 
 
 		if (col.collider.tag == "cdg2") {
 
-			Services.audioManager.Play("singing"); 
+			Services.audioManager.Play ("singing"); 
 
 		} 
 
 		if (col.collider.tag == "cdg3") {
 
-			Services.audioManager.Play("goodboy"); 
+			Services.audioManager.Play ("goodboy"); 
 
 		} 
 
 		if (col.collider.tag == "cdg4") {
 
-			Services.audioManager.Play("cutesound"); 
+			Services.audioManager.Play ("cutesound"); 
 
 		} 
 
 		if (col.collider.tag == "cg1") {
 
-			Services.audioManager.Play("secrets"); 
+			Services.audioManager.Play ("secrets"); 
 
 		} 
+	}
 
-	}  
-
+//	IEnumerator stopSparkles()
+//	{
+//		//yield return new WaitForSeconds (.4f); 
+//
+//		sparkle.GetComponent<ParticleSystem> ().enableEmission = false; 
+//
+//	}
 }
