ab816a4384e97071416049414dc644e1b25c72f4
Unknown <okh216@nyu.edu>
2018-09-01 14:02:46 -0400

    Springjoint scripting
    Changed Mouseoverselect so it is when cursor is over beat and clicked that spring activates

--- Assets/Flick.cs
+++ Assets/Flick.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class Flick : MonoBehaviour {
 
+    public SpringJoint2D spring;
+    public mouseoverselect mouseOver;
+    public float springOffsetx = 10;
+    public float springOffsety = 10;
+
 	// Use this for initialization
 	void Start () {
-		
+        spring = this.gameObject.GetComponent<SpringJoint2D>();
+        spring.connectedAnchor = gameObject.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        if (mouseOver.mouseIsover == true){
+            spring.enabled = true;
+        }
+        else if (mouseOver.mouseIsover == false)
+        {
+            spring.enabled = false;
+        }
+        if (spring.enabled == true)
+        {
+            Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            spring.connectedAnchor = cursorPosition; //+ new Vector2 (springOffsetx, springOffsety);
+        }
+    }
 }
