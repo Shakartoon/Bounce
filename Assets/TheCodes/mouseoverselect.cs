aeaa2ecc6fa77293f0566b8aef223a5e91785267
Unknown <okh216@nyu.edu>
2018-09-05 18:42:13 -0400

    It freezes!
    Press space when mousing over a ball.

--- Assets/TheCodes/mouseoverselect.cs
+++ Assets/TheCodes/mouseoverselect.cs
@@ -8,7 +8,7 @@ public class mouseoverselect : MonoBehaviour {
 	public float moveSpeed = 0.5f;
     public bool mouseIsover = false;
     public Rigidbody2D rb2d;
-    private bool isFrozen;
+    private bool isFrozen = false;
 	public GameObject greenCircle; 
 
 	// Use this for initialization
@@ -19,19 +19,6 @@ public class mouseoverselect : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            transform.position = mousePosition;
-            rb2d.simulated = false;
-            isFrozen = true;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space) && (isFrozen == true))
-        {
-            rb2d.simulated = true;
-            isFrozen = false;
-        }
-
     }
 
 	void OnMouseDown() { 
@@ -44,20 +31,35 @@ public class mouseoverselect : MonoBehaviour {
         mouseIsover = true;
 		m.enabled = true;
 
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //transform.position = mousePosition;
+            rb2d.simulated = false;
+            isFrozen = true;
+        }
+
+        //if (Input.GetKeyDown(KeyCode.Space) && (isFrozen == true))
+        //{
+        //    rb2d.simulated = true;
+        //    isFrozen = false;
+        //}
+
+
         //Enable the two finger thing? 
 
-            // Find the mesh renderer "greenCricle1" and enable. DID IT BITCH 
+        // Find the mesh renderer "greenCricle1" and enable. DID IT BITCH 
 
 
-            // if (OnMouseOver == true) {
+        // if (OnMouseOver == true) {
 
-            //Allow Double Mouse movement to happen 
-            // If not, do not allow 
+        //Allow Double Mouse movement to happen 
+        // If not, do not allow 
 
-            // } 
-    } 
+        // } 
+    }
 
-	void OnMouseUp () { 
+    void OnMouseUp () { 
 		Debug.Log ("Yes it did"); 
 		SpriteRenderer m = greenCircle.GetComponent<SpriteRenderer> ();
         mouseIsover = false;
@@ -65,6 +67,22 @@ public class mouseoverselect : MonoBehaviour {
 
 		// if two fingers, move 
 	
-	} 
-		
+	}
+    void OnMouseOver()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //transform.position = mousePosition;
+            this.rb2d.simulated = false;
+            isFrozen = true;
+        }
+
+        //if (Input.GetKeyDown(KeyCode.Space) && (isFrozen == true))
+        //{
+        //    rb2d.simulated = true;
+        //    isFrozen = false;
+        //}
+
+    }
 }
