7ec4d479decb101f6cbcf9f5139a3bac558eb5ae
Unknown <okh216@nyu.edu>
2018-09-09 13:26:18 -0400

    Added a pause button too

---
+++ Assets/TheCodes/Pausebutton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pausebutton : MonoBehaviour {
+
+    GameObject[] balls;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (balls == null)
+        {
+            balls = GameObject.FindGameObjectsWithTag("beatball");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            foreach (GameObject beat1 in balls)
+            {
+                beat1.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+            }
+        }
+    }
+}
