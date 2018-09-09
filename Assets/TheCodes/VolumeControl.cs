ec9a250133785c1a66d2006e746a06b3f818a7ab
Unknown <Sm7684@nyu.edu>
2018-09-09 03:19:36 -0400

    new scene added
    --

---
+++ Assets/TheCodes/VolumeControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic; 
+using UnityEngine; 
+
+public class VolumeControl : MonoBehaviour {
+
+	public float gain; 
+	public float volume = 0.1f;  
+
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			gain = volume; 
+		
+		}
+
+		if (Input.GetKeyUp (KeyCode.Space)) {
+
+			gain = 0; 
+		}
+
+	}
+}
