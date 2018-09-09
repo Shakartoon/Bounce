e65da36e13fc9f273ce1b76a59b3aef4896601a1
Unknown <okh216@nyu.edu>
2018-09-09 12:42:54 -0400

    Press "Z" to unfreeze all balls at the same time

---
+++ Assets/TheCodes/LoopSoundinmotion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopSoundinmotion : MonoBehaviour {
+
+    public bool isPlaying;
+    public Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Assets/TheCodes/PlayButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButton : MonoBehaviour {
+    GameObject[] balls;
+
+	// Use this for initialization
+	void Start () {
+        if (balls == null)
+        {
+            balls = GameObject.FindGameObjectsWithTag("beatball");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+           foreach (GameObject beat1 in balls)
+            {
+                beat1.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+            }
+            
+        }
+	}
+}
