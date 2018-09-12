07146fe2c08b14ed6222686742f86419c40f9e0b
Unknown <okh216@nyu.edu>
2018-09-12 15:12:06 -0400

    Began trying to code a puzzle

--- Assets/TheCodes/CirclePuzzleCollisions.cs
+++ Assets/TheCodes/CirclePuzzleCollisions.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class CirclePuzzleCollisions : MonoBehaviour {
 
+    public bool solved;
+    public bool oneKissingtwo;
+    public bool threeKissingfour;
+
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,11 +16,20 @@ public class CirclePuzzleCollisions : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
+
+        if (solved == true)
+        {
+            Debug.Log("Solved!");
+        }
 	}
 
     void OnCollisionEnter2D (Collision2D col)
     {
-
+        if (col.collider.tag == "Circlekeypart2")
+        {
+            oneKissingtwo = true;
+        }
+        
     }
 }
---
+++ Assets/TheCodes/PuzzleManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleManager : MonoBehaviour
+{
+    public bool solved;
+    public bool oneKissingtwo;
+    public bool threeKissingfour;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((oneKissingtwo == true) && (threeKissingfour == true))
+        {
+            solved = true;
+        }
+
+        if (solved == true)
+        {
+            Debug.Log("Solved!");
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.tag == "Circlekeypart2")
+        {
+            oneKissingtwo = true;
+        }
+
+        if (col.collider.tag == "Circlekeypart3")
+        {
+            threeKissingfour = true;
+        }
+
+    }
+}
+
